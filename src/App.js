7a74fc44bd41df51452ff0070ddc3f3187c923d0
unknown <rmtestard@gmail.com>
2019-12-14 18:27:49 +0100

    I have created a shop page and a collectionPreview component, we render data from shop to collectionPreview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Homepage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
+
+import Homepage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route exact path="/shop" component={Shop} />
         <Route exact path="/" component={Homepage} />
       </Switch>
     </div>
---
+++ src/components/collection/collection.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./collection.style.scss";
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items.map(item => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
