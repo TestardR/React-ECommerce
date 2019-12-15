d689efd6790a0081c22ac069904239de5bb88045
unknown <rmtestard@gmail.com>
2019-12-15 15:10:51 +0100

    I have added a header component and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/shop" component={Shop} />
         <Route exact path="/" component={Homepage} />
