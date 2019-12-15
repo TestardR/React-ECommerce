4b7449bef4644fc83c4467e4521c39a175fa5ebb
unknown <rmtestard@gmail.com>
2019-12-15 19:25:00 +0100

    I have added a form component, a form input, and a custom button

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignAndSignOut from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/shop" component={Shop} />
         <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/signin" component={SignAndSignOut} />
       </Switch>
     </div>
   );
