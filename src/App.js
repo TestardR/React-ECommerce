52bcf0825e6b6e02671ca79ea7bf217413d468e4
unknown <rmtestard@gmail.com>
2019-12-15 14:54:44 +0100

    I have updated our app with a collection-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import "./App.css";
+
 import Homepage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection/collection.component";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class Shop extends Component {
   constructor(props) {
